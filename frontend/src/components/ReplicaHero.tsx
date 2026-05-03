4b8882312c970c67f7a005154180c186cad15f05
fatburyn-6943 <fatburyn@gmail.com>
2026-05-03 08:17:29 +0530

    style: zoom out hero images using object-fit contain

--- frontend/src/components/ReplicaHero.tsx
+++ frontend/src/components/ReplicaHero.tsx
@@ -54,7 +54,7 @@ export default function ReplicaHero({
         <img 
             src={finalImageSrc} 
             alt="Hero Background" 
-            style={{ position: 'absolute', inset: 0, width: '100%', height: '100%', objectFit: 'cover', objectPosition: 'left center' }} 
+            style={{ position: 'absolute', inset: 0, width: '100%', height: '100%', objectFit: 'contain', objectPosition: 'left center' }} 
           />
       </div>
 
